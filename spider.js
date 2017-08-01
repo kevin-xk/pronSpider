a230e864b903df5cbfdff825281209963ac3bf62
Kevinzhang29 <17602180307@163.com>
2017-08-01 22:58:44 +0800

    up

--- spider.js
+++ spider.js
@@ -5,32 +5,27 @@ const fs = require('fs');
 charset(superagent);
 
 let  url= 'http://sex.corephi.com/forum/space.php?uid=12737809&mod=mythreads&page=';
+let items = [];
 
 
-let start = (url) => {
-  let items=[];
-  superagent.get(url)
-    .end((err, sres) => {
-      
-      if(err) console.log(err);
 
-      let $ = cheerio.load(sres.text, {decodeEntities: false});
-     
-      $('.subject a:first-child').each((idx , element) =>{
-        let $element = $(element);
+bbb()
 
-        items.push({
-          href: $element.attr('href')
-        })
-      })
-      /*
-      fs.appendFile('data.json', JSON.stringify(items, null, 4), (err) => {
-        if(err) console.log(err)
-      })
-    */
-    })
-    return items;
+
+//aaa(url, items)
+
+
+
+
+function page() {
+  let pageArr = [];
+  for(let i =0; i<36; i++) {
+      pageArr .push(url+i);
+  }
+  return pageArr;
 }
+
+
 
 function start2(url, items) {
   return new Promise(function(resolve,reject) {
@@ -51,47 +46,18 @@ function start2(url, items) {
     })
   })
  };
-//7.31 bug已解决
- async function aaa(url, items) {
-  await start2(url, items);
- }
 
-function bbb (url) {
-  let items = [];
-  let _page = page(url);
-  spider(_page, items);
-  fs.writeFile('data.json', JSON.stringify(items, null ,4 ) , (err) => {
+async function bbb() {
+  let a = page();
+  for(let i=0; i<a.length; i++) {
+    await aaa(a[i], items);
+  }
+  fs.writeFile('data.json', JSON.stringify(items, null, 4) , (err) => {
     if(err) console.log(err);
   })
 }
 
-bbb(url)
-
-
-function page(url) {
-  let pageArr = [];
-  for(let i =0; i<36; i++) {
-      pageArr .push(url+i);
-  }
-  return pageArr;
-}
-
-function spider (arr, items) {
-    for(let i =0; i<arr.length; i++) {
-     aaa(arr[i], items)
-    }
-}
-/*
-async function go(url) {
-  let getPage = await page(url);
-  let getspider = await spider(getPage);
-
-  return console.log('123');
-}
-
-go(url).then( (e) => {
-  console.log(e)
-})
-*/
-
-
+//7.31 bug已解决
+ async function aaa(url, items) {
+  await start2(url, items);
+ }
